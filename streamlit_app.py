9466ee6eb649ebe15713488d027bbb64a3453c0b
RikyGiava <riccardo.giavarini@mail.polimi.it>
2026-04-07 09:07:45 +0000

    Other changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 st.title("MY WEB APPLICATION")
 st.text("Yeh, this is really my first app",text_alignment="right")
+
+data = st.slider("Select a specific value", 0,30,15)
+
+st.write("The selected value is:",data)
